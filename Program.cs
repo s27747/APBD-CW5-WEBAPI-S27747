76cf1b4f375ac44a3841d2552bb1b258a9b533aa
Jakub Erbetowski <s27747@pjwstk.edu.pl>
2026-04-22 20:03:24 +0200

    initialize asp.not core web api project

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using APBD_CW5_S27747.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+AppData.Initialize();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
